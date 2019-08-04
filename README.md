fa9911ad1ada0b076b89f61e0db5c0ba7d034050
kycheng <im_jack@qq.com>
2019-08-05 01:08:21 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
+MF-TAPNet
 =============
-NOTE: Update in progress
 
+[TOC]
 
-MICCAI 2017 Endoscopic vision challenge
-https://endovissub2017-roboticinstrumentsegmentation.grand-challenge.org/
+----------------------------------------------------
 
-Dataset and instructions
-https://endovissub2017-roboticinstrumentsegmentation.grand-challenge.org/Data/
+NOTE: This repository is still updating in progress
+------
 
-Download dataset and summary report:
-https://endovissub2017-roboticinstrumentsegmentation.grand-challenge.org/Downloads/
+Useful Links
+------
+
+[MICCAI 2017 Endoscopic vision challenge](https://endovissub2017-roboticinstrumentsegmentation.grand-challenge.org/)
+
+[Dataset and instructions](
+https://endovissub2017-roboticinstrumentsegmentation.grand-challenge.org/Data/)
+
+[Download dataset and report summary](https://endovissub2017-roboticinstrumentsegmentation.grand-challenge.org/Downloads/)
+
 As required, you need to register an account and get the permission to download the dataset.
 
 
-Requirements:
+Instructions
+------
+
+To train and evaluate the TF-MAPNet model, you may follow the instructions.
+
+
+### Dependencies
+
+```
 - Python 3.6
 - pytorch 0.4.1+
 - pytorch-ignite
@@ -21,106 +37,85 @@ Requirements:
 - albumentations
 - opencv-python
 - cupy
+```
 
+### Folder Structure
 
 The structure of this project will be arranged as follows:
 
+```
 (root folder)
-|-data
-|  |-train
-|  |  |-instrument_dataset_1
-|  |  |  |-left_frames
-|  |  |  |-right_frames
-|  |  |  |-......
-|  |  |-instrument_dataset_2
-|  |  |  |-left_frames
-|  |  |  |-right_frames
-|  |  |  |-......
-|  |-cropped_train
-|-src
-|-pretrained_model
-|  |-network-css.pytorch
-|-......
+├── data
+|  ├── train
+|  |  ├── instrument_dataset_1
+|  |  |  ├── left_frames
+|  |  |  ├── right_frames
+|  |  |  ├── ......
+|  |  ├── instrument_dataset_2
+|  |  |  ├── left_frames
+|  |  |  ├── right_frames
+|  |  |  ├── ......
+|  ├── cropped_train
+├── src
+├── pretrained_model
+|  ├── network-css.pytorch
+├── ......
+```
+
+### Download prerequisites
 
 
+* Assume the working directory is ``$ROOT_DIR/``. Download the train dataset (2 zips including instrument_dataset 1 to 8). Unzip ``instrument_dataset_X`` into ``data/train`` following the above structure. The train dataset should be arranged as ``$ROOT_DIR/data/train/instrument_dataset_X/...``.
 
-Instructions
+```
+$ git clone https://github.com/keyuncheng/MF-TAPNet.git
+$ cd MF-TAPNet
+$ mkdir data/
+$ mkdir data/train
+$ unzip instrument_1_4_training.zip -d data/train
+$ unzip instrument_5_8_training.zip -d data/train
+```
 
-The follow instructions are valid under Ubuntu
+* Download UnFlow pytorch pretrained model for optical flow estimation, then move it to ``$ROOT_DIR/pretrained_model/``.
 
-Download project and dataset
+```
+$ wget --timestamping http://content.sniklaus.com/github/pytorch-unflow/network-css.pytorch
+$ mkdir pretrained_model
+$ mv network-css.pytorch pretrained_model
+```
 
-- we assume the current directory as $ROOT_DIR/
-- download the training dataset (2 zips including instrument_dataset_1-8)
-- unzip instrument_dataset_X into data/train (X=1-8) following the above structure
-- the training dataset should be arranged as $ROOT_DIR/data/train/instrument_dataset_X/...
+* Switch to source code folder
 
-- $ git clone https://github.com/keyuncheng/MF-TAPNet.git
-- $ cd MF-TAPNet
-- $ mkdir data/
-- $ mkdir data/train
-- $ unzip instrument_1_4_training.zip -d data/train
-- $ unzip instrument_5_8_training.zip -d data/train
+```
+$ cd src/
+```
 
+### Preprocess data
 
-Download pretrained UnFlow pytorch model
+```
+$ python preprocess_data.py
+```
 
-- download UnFlow pytorch pretrained model and move it to $ROOT_DIR/pretrained_model/
+* Choose GPUs
 
+```
+$ export CUDA_VISIBLE_DEVICES=a,b,c,d (this could be changed according to training details)
+```
 
-- $ wget --timestamping http://content.sniklaus.com/github/pytorch-unflow/network-css.pytorch
-- $ mkdir pretrained_model
-- $ mv network-css.pytorch pretrained_model
+* Estimate optical flow for image pairs
 
+We use pretrained UnFlow to estimate optical flow for consecutive image pairs in each surgical video. This step is tricky because the UnFlow model are trained with other datasets (KITTI, 1280 * 384) but we are trying to estimate the optical flow using surgical videos frames in different sizes. In addition, we cannot train-from-scratch/finetune the UnFlow model in a supervised way without dense optical flow as ground truths. For more accurate optical flow estimation, we are trying other methods (unsupervised fine-tuning using surgical videos).
 
-switch to source code folder
-- $ cd src/
+```
+$ python gen_optflow.py
+```
 
+### Train the model
 
-Preprocess data
+Arguments for model training in ``train.sh`` are in default settings. You may try other models by modifying it.
 
-- $ python preprocess_data.py
+```
+$ sh train.sh
+```
 
-
-Choose GPUs
-
-export CUDA_VISIBLE_DEVICES=a,b,c,d (this could be changed according to training details)
-
-
-calculate optical flow for each two image pairs using UnFlow
-
-- this step is tricky because the original size of input should not be self-defined,
-instead should be like the one it was pre-trained with other datasets (KITTI, 1280 * 384)
-- since we cannot train it directly given no optical flow ground truths of our dataset
-- for better results of calculating optical flow, we may consider other ways
-
-- $ python gen_optflow.py
-
-model training
-
-- arguments are defined in train.sh and can be modified
-
-- $ sh train.sh
-
-
-----------------------------------------------------------------------------------------
-Problems:
-1. how to deal with attention maps when training paused and restored?
-2. Sigmoid and Softmax in Attention Module (Not trained)
-3. bn in train and valid
-4. (IMPORTANT) whether using softmax operation in attention module or not
-5. (IMPORTANT)use + or * or conv2d(kernel 1) in final attention module operation
-
-Handle:
-1. Dice in binary classification
-2. metrics for multiclass classification (Not Debuged)
-3. larger Attention map
-4. multiple config.py (for preprocess_data.py)
-5. Semi-supervised model
-
-
-TRAIN_RECORD for each model:
-Binary: 
-1. UNet (not resized) batchsize=8, learn_rate=1e-5, epoches=20, gpu=4 (TITAN V), validation IoU=0.6728385997379875: validation Dice=0.784111333214352
-2. UNet11 (not resized) batchsize=8, learn_rate=1e-5, epoches=20, gpu=4 (TITAN V), validation IoU=0.8175544938356124: validation Dice=0.8885956894127561
-2. UNet16 (not resized) batchsize=8, learn_rate=1e-5, epoches=20, gpu=4 (TITAN V), validation IoU=: validation Dice=
+### Still updating
--- src/ds_utils/robseg_2017.py
+++ src/ds_utils/robseg_2017.py
@@ -146,93 +146,89 @@ def preprocess_data(args):
     cropped_data_dir.mkdir(exist_ok=True, parents=True)
 
     # max number of folders
-    max_num_folders = 8 if args.mode == 'train' else 10
+    num_folders = 8 if args.mode == 'train' else 10
 
-    # only read left frames
-    # crop (height, width) frames from (h_start, w_start)
-    global h_start, w_start
-    global cropped_height, cropped_width
-    global problem_factor
+    global problem_factor, mask_folder
+    # crop (cropped_height, cropped_width) from (h_start, w_start)
+    global cropped_height, cropped_width, h_start, w_start
 
 
-    for idx in range(1, max_num_folders + 1):
-        cropped_instrument_folder = cropped_data_dir / ('instrument_dataset_' + str(idx))
-
-        # mkdir for each datatype
-        image_folder = cropped_instrument_folder / 'images'
-        image_folder.mkdir(exist_ok=True, parents=True)
-
-        binary_mask_folder = cropped_instrument_folder / 'binary_masks'
-        binary_mask_folder.mkdir(exist_ok=True, parents=True)
-
-        parts_mask_folder = cropped_instrument_folder / 'parts_masks'
-        parts_mask_folder.mkdir(exist_ok=True, parents=True)
-
-        instrument_mask_folder = cropped_instrument_folder / 'instruments_masks'
-        instrument_mask_folder.mkdir(exist_ok=True, parents=True)
-
+    for idx in range(1, num_folders + 1):
         # original dataset dir
         instrument_folder = data_dir / ('instrument_dataset_' + str(idx))
 
+        # original mask folder
+        ori_mask_folders = list((instrument_folder / 'ground_truth').glob('*'))
 
-        # mask folder
-        mask_folders = list((instrument_folder / 'ground_truth').glob('*'))
-
-        # frames dir
+        # video frames dir (only read left frames)
         frames_dir = instrument_folder / 'left_frames'
+
+        # processed dataset dir
+        processed_instrument_folder = cropped_data_dir / ('instrument_dataset_' + str(idx))
+
+        # mkdir for each problem_type
+        image_folder = processed_instrument_folder / 'images'
+        image_folder.mkdir(exist_ok=True, parents=True)
+
+        binary_mask_folder = processed_instrument_folder / mask_folder['binary']
+        binary_mask_folder.mkdir(exist_ok=True, parents=True)
+
+        parts_mask_folder = processed_instrument_folder / mask_folder['parts']
+        parts_mask_folder.mkdir(exist_ok=True, parents=True)
+
+        instrument_mask_folder = processed_instrument_folder / mask_folder['instruments']
+        instrument_mask_folder.mkdir(exist_ok=True, parents=True)
+
         for file_name in tqdm.tqdm(list(frames_dir.glob('*')),
             desc='preprocess dataset %d' % idx, dynamic_ncols=True):
             img = cv2.imread(str(file_name))
             old_h, old_w, _ = img.shape
 
             img = img[h_start: h_start + cropped_height, w_start: w_start + cropped_width]
-            # save cropped image
-            # cv2.imwrite(str(image_folder / (file_name.stem + '.jpg')), img,
-            #             [cv2.IMWRITE_JPEG_QUALITY, 100])
+            # save cropped frame
             cv2.imwrite(str(image_folder / (file_name.name)), img)
-
-            # save different masks
+            # create empty masks
             mask_binary = np.zeros((old_h, old_w))
             mask_parts = np.zeros((old_h, old_w))
             mask_instruments = np.zeros((old_h, old_w))
 
-            for mask_folder in mask_folders:
+            for ori_mask_folder in ori_mask_folders:
                 # read in grayscale
-                mask = cv2.imread(str(mask_folder / file_name.name), 0)
+                mask = cv2.imread(str(ori_mask_folder / file_name.name), 0)
 
                 # mark each type of instruments
                 # background will be set to 0 in default
-                if 'Bipolar_Forceps' in str(mask_folder):
+                if 'Bipolar_Forceps' in str(ori_mask_folder):
                     mask_instruments[mask > 0] = 1
-                elif 'Prograsp_Forceps' in str(mask_folder):
+                elif 'Prograsp_Forceps' in str(ori_mask_folder):
                     mask_instruments[mask > 0] = 2
-                elif 'Large_Needle_Driver' in str(mask_folder):
+                elif 'Large_Needle_Driver' in str(ori_mask_folder):
                     mask_instruments[mask > 0] = 3
-                elif 'Vessel_Sealer' in str(mask_folder):
+                elif 'Vessel_Sealer' in str(ori_mask_folder):
                     mask_instruments[mask > 0] = 4
-                elif 'Grasping_Retractor' in str(mask_folder):
+                elif 'Grasping_Retractor' in str(ori_mask_folder):
                     mask_instruments[mask > 0] = 5
-                elif 'Monopolar_Curved_Scissors' in str(mask_folder):
+                elif 'Monopolar_Curved_Scissors' in str(ori_mask_folder):
                     mask_instruments[mask > 0] = 6
-                elif 'Other' in str(mask_folder):
+                elif 'Other' in str(ori_mask_folder):
                     mask_instruments[mask > 0] = 7
 
                 # process dir exclude 'Other_labels'
-                if 'Other' not in str(mask_folder):
+                if 'Other' not in str(ori_mask_folder):
                     # if exists, will be add in
                     mask_binary += mask
 
-                    # different parts
                     mask_parts[mask == 10] = 1  # Shaft
                     mask_parts[mask == 20] = 2  # Wrist
                     mask_parts[mask == 30] = 3  # Claspers
 
-            mask_binary = (mask_binary[h_start: h_start + cropped_height, w_start: w_start + cropped_width] > 0).astype(
-                np.uint8) * problem_factor["binary"]
-            mask_parts = (mask_parts[h_start: h_start + cropped_height, w_start: w_start + cropped_width]).astype(
-                np.uint8) * problem_factor["parts"]
-            mask_instruments = (mask_instruments[h_start: h_start + cropped_height, w_start: w_start + cropped_width]).astype(
-                np.uint8) * problem_factor["instruments"]
+            # crop and save masks
+            mask_binary = (mask_binary[h_start: h_start + cropped_height, 
+                w_start: w_start + cropped_width] > 0).astype(np.uint8) * problem_factor["binary"]
+            mask_parts = (mask_parts[h_start: h_start + cropped_height, 
+                w_start: w_start + cropped_width]).astype(np.uint8) * problem_factor["parts"]
+            mask_instruments = (mask_instruments[h_start: h_start + cropped_height,
+                w_start: w_start + cropped_width]).astype(np.uint8) * problem_factor["instruments"]
 
             cv2.imwrite(str(binary_mask_folder / file_name.name), mask_binary)
             cv2.imwrite(str(parts_mask_folder / file_name.name), mask_parts)
